00dc2d8d47c635b2a221fb6852a1cbd2efe7d198
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-10-19 01:11:43 +0900

    check_rds

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "UMC Server WorkBook API", version = "1.0.0", description = "UMC Server WorkBook API 명세서"))
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI UMCstudyAPI() {
+        String jwtSchemeName = "JWT TOKEN";
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .components(components);
+    }
+}
